043810e117a46af5d313d93fb58bf3b51901e5e0
Mikhail VV <esto045@gmail.com>
2022-07-19 10:58:04 +0300

    add list

--- BaseDDT/BaseDDT/Program.cs
+++ BaseDDT/BaseDDT/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Text;
+
+
+/*
+ * user Manual
+ * Выход                                                  exit
+ * Вывод всех строк                                       selectall
+ * Сортировка                                             sortby <column> <asc/desc>
+ * Поиск                                                  search <column> <text>
+ * Минимальное значение колонки "Средний балл"            min
+ * Максимальное значение колонки "Средний балл"           max
+ * Среднее значение колонки "Средний балл"                avg
+ * Сумма значение колонки "Средний балл"                  sum
+ * Очистка экрана                                         clear
+ * Удаление                                               DELETE FROM <table_name> WHERE <condition>
+ * Добавление                                             INSERT [INTO] имя_таблицы [(список_столбцов)] VALUES (значение1,значение2 ... )
+ * Изменение                                              UPDATE <имя_таблицы> SET<имя столбца> = <значение> WHERE condition
+ */
 
 namespace BaseDDT
 {
     class Program
     {
 
-        private static string connectionString = ConfigurationManager.ConnectionStrings["StudentDB"].ConnectionString;
+        private static string connectionString = ConfigurationManager.ConnectionStrings["StudentDB1"].ConnectionString;
 
         private static SqlConnection sqlConnection = null;
+
+        static string command = string.Empty;
+
+        static string result = string.Empty;
+
+        static string[] commandArray;
+
+        private static void WriteToFile()
+        {
+            using (StreamWriter sW = new StreamWriter(
+                                    $"{AppDomain.CurrentDomain.BaseDirectory}/{commandArray[0]}_{DateTime.Now.ToString().Replace(':', '-')}.txt",
+                                    true, Encoding.UTF8))
+            {
+                sW.WriteLine(DateTime.Now.ToString());
+
+                sW.WriteLine(command);
+
+                sW.WriteLine(result);
+            }
+        }
+
         static void Main(string[] args)
         {
             sqlConnection = new SqlConnection(connectionString);
@@ -19,7 +59,6 @@ namespace BaseDDT
 
             SqlDataReader sqlDataReader = null;
 
-            string command = string.Empty;
             
             while(true)
             {
@@ -53,50 +92,64 @@ namespace BaseDDT
 
                     //SELECT * FROM [Students] WHERE Id=1
 
-                string[] commandArray = command.ToLower().Split(' ');
+                 commandArray = command.ToLower().Split(' ');
 
                 switch (commandArray[0])
                     {
+                        case "fselectall":
                         case "selectall":
                             sqlCommand = new SqlCommand("SELECT * FROM [Students]", sqlConnection);
                             sqlDataReader = sqlCommand.ExecuteReader();
 
                             while (sqlDataReader.Read())
                             {
-                                Console.WriteLine($"{sqlDataReader["Id"]}" +
+                                result += $"{sqlDataReader["Id"]}" +
                                                   $"{sqlDataReader["FIO"]}" +
                                                   $"{sqlDataReader["Birthday"]}" +
                                                   $"{sqlDataReader["University"]}" +
                                                   $"{sqlDataReader["Group_number"]}" +
-                                                  $"{sqlDataReader["Average_score"]}"
-                                                  );
-                                Console.WriteLine(new string('-', 30));
+                                                  $"{sqlDataReader["Average_score"]}\n";
+                                result += new string('-', 30) + "\n";
                             }
 
                             if (sqlDataReader != null)
                             {
                                 sqlDataReader.Close();
                             }
+
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();                              
+                            }
                             break;
+                        case "fselect":
                         case "select":
                             sqlCommand = new SqlCommand(command, sqlConnection);
                             sqlDataReader = sqlCommand.ExecuteReader();
 
                             while (sqlDataReader.Read())
                             {
-                                Console.WriteLine($"{sqlDataReader["Id"]}" +
+                                result += $"{sqlDataReader["Id"]}" +
                                                   $"{sqlDataReader["FIO"]}" +
                                                   $"{sqlDataReader["Birthday"]}" +
                                                   $"{sqlDataReader["University"]}" +
                                                   $"{sqlDataReader["Group_number"]}" +
-                                                  $"{sqlDataReader["Average_score"]}" 
-                                                  );
-                                Console.WriteLine(new string('-', 30));
+                                                  $"{sqlDataReader["Average_score"]}\n";
+                                result += new string('-', 30) + "\n";
                             }
 
-                            if(sqlDataReader != null)
+                            if (sqlDataReader != null)
                             {
                                 sqlDataReader.Close();
+                            }
+
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();
                             }
 
                             break;
@@ -116,6 +169,7 @@ namespace BaseDDT
                                 Console.WriteLine($"Удалено: {sqlCommand.ExecuteNonQuery()} строк(а)");
                     
                             break;
+                        case "fsortby":
                         case "sortby":
                                 //sortby fio asc
 
@@ -125,23 +179,140 @@ namespace BaseDDT
 
                                 while (sqlDataReader.Read())
                                 {
-                                    Console.WriteLine($"{sqlDataReader["Id"]}" +
-                                                      $"{sqlDataReader["FIO"]}" +
-                                                      $"{sqlDataReader["Birthday"]}" +
-                                                      $"{sqlDataReader["University"]}" +
-                                                      $"{sqlDataReader["Group_number"]}" +
-                                                      $"{sqlDataReader["Average_score"]}"
-                                                      );
-                                    Console.WriteLine(new string('-', 30));
-                                }
+                                result += $"{sqlDataReader["Id"]}" +
+                                               $"{sqlDataReader["FIO"]}" +
+                                               $"{sqlDataReader["Birthday"]}" +
+                                               $"{sqlDataReader["University"]}" +
+                                               $"{sqlDataReader["Group_number"]}" +
+                                               $"{sqlDataReader["Average_score"]}\n";
+                                result += new string('-', 30) + "\n";
+                            }
 
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
+
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();
+                            }
+
+                            break;
+                        case "fsearch":
+                        case "search":
+                            if (commandArray[1].Equals("fio"))
+                            {
+                                sqlCommand = new SqlCommand($"SELECT * FROM [Students] WHERE FIO LIKE N'%{commandArray[2]}%'", sqlConnection);
+                            }
+                            else if (commandArray[1].Equals("course"))
+                            {
+                                sqlCommand = new SqlCommand($"SELECT * FROM [Students] WHERE Course ='{commandArray[2]}'", sqlConnection);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine($"аргумент {commandArray[1]} некорректен!");
+                            }
+
+                            try
+                            {
+                                sqlDataReader = sqlCommand.ExecuteReader();
+
+                                while (sqlDataReader.Read())
+                                {
+                                    result += $"{sqlDataReader["Id"]}" +
+                                                  $"{sqlDataReader["FIO"]}" +
+                                                  $"{sqlDataReader["Birthday"]}" +
+                                                  $"{sqlDataReader["University"]}" +
+                                                  $"{sqlDataReader["Group_number"]}" +
+                                                  $"{sqlDataReader["Average_score"]}\n";
+                                result += new string('-', 30) + "\n";
+                            }
+
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
+
+                            }
+                            catch(Exception ex)
+                            {
+                                Console.WriteLine($"Ошибка: {ex.Message}");
+                            }
+                            finally
+                            {
                                 if (sqlDataReader != null)
                                 {
                                     sqlDataReader.Close();
                                 }
 
-                                break;
+                                Console.WriteLine(result);
+
+                                if (commandArray[0][0] == 'f')
+                                {
+                                    WriteToFile();
+                                }
+                            }
+                            break;
+                        case "fmin":
+                        case "min":
+
+                            sqlCommand = new SqlCommand("SELECT MIN(Average_score) FROM [Students]", sqlConnection);
+
+                            result =($"Минимальный средний балл : {sqlCommand.ExecuteScalar()}");
+
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();                      
+                            }
+
+                            break;
+                        case "fmax":
+                        case "max":
+
+                            sqlCommand = new SqlCommand("SELECT MAX(Average_score) FROM [Students]", sqlConnection);
+
+                            result = ($"Максимальный средний балл : {sqlCommand.ExecuteScalar()}");
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();
+                            }
+                            break;
+                        case "favg":
+                        case "avg":
+                            sqlCommand = new SqlCommand("SELECT AVG(Average_score) FROM [Students]", sqlConnection);
+
+                            result = ($"Минимальный средний балл : {sqlCommand.ExecuteScalar()}");
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();
+                            }
+                            break;
+                        case "fsun":
+                        case "sum":
+                            sqlCommand = new SqlCommand("SELECT SUM(Average_score) FROM [Students]", sqlConnection);
+
+                            result = ($"Минимальный средний балл : {sqlCommand.ExecuteScalar()}");
+                            Console.WriteLine(result);
+
+                            if (commandArray[0][0] == 'f')
+                            {
+                                WriteToFile();
+                            }
+                            break;
+
                         default:
+
+                         
                             Console.WriteLine($"Команда {command} некорректна!");
                             break;
 
@@ -151,7 +322,7 @@ namespace BaseDDT
                 {
                     Console.WriteLine($"ошибка {ex.Message}");
                 }
-
+                result = string.Empty;
             }
 
             Console.WriteLine("najhmi knopku");
---
+++ BaseDDT/dbo.Students.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Students] ON
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (1, N'Васильев Михаил Юрьевич', N'1993-06-11', N'Ургпу', N'123', 1, 1)
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (2, N'Васильева Анастасия', N'1997-05-08', N'Урфу', N'543', 1, 1)
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (10, N'Юрий Трифонов', N'1992-06-13', N'Урфу', N'433', 1, 1)
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (11, N'Андрей Янн', N'1994-05-27', N'Урфу', N'553', 2, 1)
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (12, N'Евгения Бабикова', N'1996-01-15', N'Урфу', N'542', 4, 1)
+INSERT INTO [dbo].[Students] ([Id], [FIO], [Birthday], [University], [Group_number], [Course], [Average_score]) VALUES (13, N'Артем Курицын', N'1996-04-30', N'Урфу', N'111', 4, 1)
+SET IDENTITY_INSERT [dbo].[Students] OFF
